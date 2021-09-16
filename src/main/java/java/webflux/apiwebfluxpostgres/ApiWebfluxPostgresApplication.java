3eff95843eda584493fdb323312aac9b0fd1d651
Alexander Merino <gamerino@pichincha.com>
2021-09-16 12:46:11 -0500

    Configuration project

---
+++ src/main/java/java/webflux/apiwebfluxpostgres/ApiWebfluxPostgresApplication.java
@@ -0,0 +1,13 @@
+package java.webflux.apiwebfluxpostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWebfluxPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWebfluxPostgresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/java/webflux/apiwebfluxpostgres/ApiWebfluxPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package java.webflux.apiwebfluxpostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWebfluxPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
